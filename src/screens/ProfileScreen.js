13e5e5369632fe209f227743bf5405c8623b08d4
Rangga Prastio <ranggaprastio123@gmail.com>
2019-10-11 10:34:32 +0700

    Adding image picker function

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -18,7 +18,7 @@ class ProfileScreen extends Component {
       <View style={styles.container}>
         <View style={styles.profileTop}>
           <View style={styles.imgFrame}>
-            <Image style={styles.imgProfile} resizeMode="contain" source={{uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d9b1f8f1-a3ec-432d-8e5e-91793bd37cc4/dcf7g8s-654d8a71-547a-4337-904f-7e73bdfe1b85.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q5YjFmOGYxLWEzZWMtNDMyZC04ZTVlLTkxNzkzYmQzN2NjNFwvZGNmN2c4cy02NTRkOGE3MS01NDdhLTQzMzctOTA0Zi03ZTczYmRmZTFiODUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.LysuaIEvv3cWHbj-Ase46NR55Wjfs_Xphta4RjNuSSw"}} />
+            <Image style={styles.imgProfile} resizeMode="cover" source={{uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d9b1f8f1-a3ec-432d-8e5e-91793bd37cc4/dcf7g8s-654d8a71-547a-4337-904f-7e73bdfe1b85.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q5YjFmOGYxLWEzZWMtNDMyZC04ZTVlLTkxNzkzYmQzN2NjNFwvZGNmN2c4cy02NTRkOGE3MS01NDdhLTQzMzctOTA0Zi03ZTczYmRmZTFiODUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.LysuaIEvv3cWHbj-Ase46NR55Wjfs_Xphta4RjNuSSw"}} />
           </View>
           <Text style={styles.nameTitle}>Your Name</Text>
         </View>
